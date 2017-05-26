278e41e632958567b7ef8d27f2d2d623357c76b8
seintzx <seintzx@gmail.com>
2017-05-26 09:36:30 +0200

    added git aliases

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -1,22 +1,35 @@
 export VISUAL="vim"
+
+# terminal aliases
 alias las='ls -la --color=auto'
 alias ls='ls -l --color=auto'
 alias fckbeep='xset -b'
 alias scrblk='xset s off & xset s blank & xset -dpms' #-dpms
-alias fixbeep='fckbeep & scrblk & echo fuckingdone'
+alias diocane='fckbeep & scrblk & echo fuckingdone'
 alias lump='xbacklight -inc 10'
 alias lumm='xbacklight -dec 10'
-alias sysup='yaourt -Syu'
-alias ccomp='gcc -Wall -o'
+alias susp='systemctl suspend'
 alias sshot='import -window root'
+
+# Yaourt aliases
+alias sysup='yaourt -Syu'
+
+# Compiler aliases
+alias ccomp='gcc -Wall -o'
+
+# openssl, vpn aliases
 alias aesenc='openssl aes-256-cbc -a -salt'
 alias aesdec='openssl aes-256-cbc -d -a'
-alias susp='systemctl suspend'
+alias vpnces='cd ~/vpncesena ; sudo openvpn seintz-TO-IPFire.ovpn'
+
+# git aliases
 alias gitpush='git push -u origin master'
+alias gitadal='git add --all'
+alias gitcome='git commit --allow-empty -m'
+alias gitcomm='git commit -m'
 
 
 
 
 
-
-#alias scrlock='i3lock -c 400040 -n -e' #-c: set color, -n: kill the app after unlock, -e: ignore empty input
+#alias scrlock='i3lock -c 400040 -n -e' #######-c: set color, -n: kill the app after unlock, -e: ignore empty input
