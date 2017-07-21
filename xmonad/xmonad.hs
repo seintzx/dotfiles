cd160501c9332284aebbbae823e540183539c8df
seintzx <seintzx@gmail.com>
2017-07-21 03:44:43 +0200

    updates everywhere pt2

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -63,7 +63,7 @@ myNormalBorderColor  = "#222222"
 myFocusedBorderColor = "#000000"
 
 myDefaultGaps :: [(Integer, Integer, Integer, Integer)]
-myDefaultGaps = [(0,0,0,0)]
+myDefaultGaps = [(20,0,0,0)]
 
 -- XMOBAR ======================================================================================================================================
 myLogHook :: Handle -> X ()
@@ -142,7 +142,56 @@ myLayoutPrompt = inputPromptWithCompl myXPConfig "Layout"
 
 -- KEYBINDS ==================================================================================================================================
 
-
+mykeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    -- launch terminal, default with screen
+    [ ((modm ,              xK_Return), spawn $ XMonad.terminal conf)
+    -- close focused window
+    , ((modm,               xK_q     ), kill)
+    -- Rotate through the available layout algorithms
+    , ((modm,               xK_space ), sendMessage NextLayout)
+    --  Reset the layouts on the current workspace to default
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    -- Resize viewed windows to the correct size
+    , ((modm,               xK_n     ), refresh)
+    -- Move focus to the next window
+    , ((modm,               xK_j     ), windows W.focusDown)
+    -- Move focus to the previous window
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    -- Move focus to the master window
+    , ((modm,               xK_m     ), windows W.focusMaster)
+    -- Swap the focused window and the master window
+    , ((modm,               xK_Return), windows W.swapMaster)
+    -- Swap the focused window with the next window
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    -- Swap the focused window with the previous window
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    -- Shrink the master area
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    -- Expand the master area
+    , ((modm,               xK_l     ), sendMessage Expand)
+    -- Push window back into tiling
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+    -- Increment the number of windows in the master area
+    , ((modm,               xK_comma ), sendMessage (IncMasterN 1))
+    -- Deincrement the number of windows in the master area
+    , ((modm,               xK_period), sendMessage (IncMasterN (-1)))
+    -- Quit xmonad
+    , ((modm .|. shiftMask, xK_e     ),   io (exitWith ExitSuccess))
+    -- Restart xmonad
+    , ((modm .|. shiftMask, xK_q     ),   spawn "xmonad --recompile && xmonad --restart")
+    -- Personal Keybinds
+    , ((modm .|. shiftMask, xK_z     ),   spawn "i3lock -n --image=/home/seintz/Pictures/lockscreen.png -t -e")
+    , ((shiftMask,          xK_Print),    spawn "sleep 0.2; scrot -s /home/seintz/Pictures/screenshot/scr-$(date +%Y_%m_%d)-%s.png")
+    , ((0,                  xK_Print),    spawn "scrot /home/seintz/Pictures/screenshot/scr-$(date +%Y_%m_%d)-%s.png")       
+    ]
+    ++
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. modm, k), windows $ f i)
+    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 -- MAIN ======================================================================================================================================
 
@@ -156,11 +205,9 @@ main = do
         ,   normalBorderColor   = myNormalBorderColor
         ,   focusedBorderColor  = myFocusedBorderColor
         
+        -- ,   keys                = mykeys <+> keys defaultConfig
+
         ,   manageHook          = mymanageHook <+> manageDocks <+> manageHook defaultConfig
-        ,   layoutHook          = avoidStruts  $ myLayout 
+        ,   layoutHook          = avoidStruts $ myLayout 
         ,   logHook             = myLogHook xmproc
-        }`additionalKeys`
-        [   ((mod4Mask .|. shiftMask, xK_z),    spawn "i3lock -n --image=/home/seintz/Pictures/lockscreen.png -t -e")
-        ,   ((shiftMask, xK_Print),             spawn "sleep 0.2; scrot -s /home/seintz/Pictures/screenshot/scr-$(date +%Y_%m_%d)-%s.png")
-        ,   ((0, xK_Print),                     spawn "scrot /home/seintz/Pictures/screenshot/scr-$(date +%Y_%m_%d)-%s.png")       
-        ]
+        }
