ae0504b2659e3dd465182fe091832a2cbb56a837
seintzx <seintzx@gmail.com>
2017-07-26 19:01:03 +0200

    todos

--- scripts/sha1gen.py
+++
@@ -1,5 +0,0 @@
-#Get SHA-1 hex string 
-import sha
-s = 'String to Convert'
-a = sha.new(s)
-print a.hexdigest()
--- scripts/str2hex.py
+++
@@ -1,7 +0,0 @@
-#Convert string into hex 
-
-s = 'hello world!'
-h = ''.join(hex(ord(ch))[2:] for ch in s)
-
-print "String: " + s
-print "Hex: " + h
