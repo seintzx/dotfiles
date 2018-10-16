50b1801d7f467e49f4a53665160dcf1b1d14a159
seintzx <seintzx@gmail.com>
2018-10-16 23:20:03 +0200

    back to lockscreen

--- wm_script/lockscr.sh
+++ wm_script/lockscr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # NOTE: The resize is done to speed up the process
 
-# IMAGE=${HOME}/Pictures/wall/lockscreen.png
-IMAGE=/tmp/i3lock.png
-SCREENSHOT="scrot $IMAGE"
-BLURTYPE="2x8"
-$SCREENSHOT
-convert $IMAGE -resize 50%
-convert $IMAGE -blur $BLURTYPE $IMAGE
-convert $IMAGE -resize 200%
+IMAGE=${HOME}/Pictures/wall/lockscreen.png
+# IMAGE=/tmp/i3lock.png
+# SCREENSHOT="scrot $IMAGE"
+# BLURTYPE="2x8"
+# $SCREENSHOT
+# convert $IMAGE -resize 50%
+# convert $IMAGE -blur $BLURTYPE $IMAGE
+# convert $IMAGE -resize 200%
 i3lock -n -i $IMAGE -e
-rm $IMAGE
+# rm $IMAGE
