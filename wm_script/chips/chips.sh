f009c3507da6a50f2fb272bd60a916884b287230
seintzx <seintzx@gmail.com>
2018-11-04 23:00:21 +0100

    add discord and tutanota

--- wm_script/chips/chips.sh
+++ wm_script/chips/chips.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 TRELLO="${HOME}/.config/wm_script/chips/prod/trello.sh"
 GMAIL="${HOME}/.config/wm_script/chips/mail/gmail.sh"
+TUTA="${HOME}/.config/wm_script/chips/mail/tuta.sh"
 GCAL="${HOME}/.config/wm_script/chips/mail/gcal.sh"
 WATTA="${HOME}/.config/wm_script/chips/chat/whatsapp.sh"
 SLACK="${HOME}/.config/wm_script/chips/chat/slack.sh"
@@ -8,4 +11,5 @@ SLACK="${HOME}/.config/wm_script/chips/chat/slack.sh"
 . ${SLACK}
 . ${TRELLO}
 . ${GMAIL}
+. ${TUTA}
 . ${GCAL}
--- wm_script/common.sh
+++ wm_script/common.sh
@@ -4,6 +4,7 @@ CHIPS="${HOME}/.config/wm_script/chips/chips.sh"
 
 firefox &
 megasync &
+discord &
 signal-desktop &
 telegram-desktop &
 . ${CHIPS}
