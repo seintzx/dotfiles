2d6035133cea0ba4278f67414841627648f96b4c
seintzx <seintzx@gmail.com>
2018-11-05 01:02:51 +0100

    change chips, add layout load

--- wm_script/common.sh
+++ wm_script/common.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-CHIPS="${HOME}/.config/wm_script/chips/chips.sh"
+WATTA="${HOME}/.config/wm_script/chips/chat/whatsapp.sh"
+SLACK="${HOME}/.config/wm_script/chips/chat/slack.sh"
+TRELLO="${HOME}/.config/wm_script/chips/prod/trello.sh"
+TUTA="${HOME}/.config/wm_script/chips/mail/tuta.sh"
+GMAIL="${HOME}/.config/wm_script/chips/mail/gmail.sh"
+GCAL="${HOME}/.config/wm_script/chips/mail/gcal.sh"
+
+W2="2:"
+W7="7:"
+W8="8:"
+
+i3-msg "workspace ${W7}; append_layout ${HOME}/.config/i3/7.json"
+i3-msg "workspace ${W8}; append_layout ${HOME}/.config/i3/8.json"
+i3-msg "workspace ${W2};"
 
 firefox &
-megasync &
-discord &
-signal-desktop &
+
 telegram-desktop &
-. ${CHIPS}
+signal-desktop &
+. ${WATTA}
+. ${SLACK}
+
+. ${TRELLO}
+. ${TUTA}
+. ${GMAIL}
+. ${GCAL}
