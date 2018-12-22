9e1cadbc657ff5d31f6a6d83639a28d1929df187
seintzx <seintzx@gmail.com>
2018-12-22 01:04:12 +0100

    add workspace layout, fix script

--- wm_script/common.sh
+++ wm_script/common.sh
@@ -1,31 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 WATTA="${HOME}/.config/wm_script/chips/chat/whatsapp.sh"
 SLACK="${HOME}/.config/wm_script/chips/chat/slack.sh"
 TRELLO="${HOME}/.config/wm_script/chips/prod/trello.sh"
 TUTA="${HOME}/.config/wm_script/chips/mail/tuta.sh"
-GMAIL="${HOME}/.config/wm_script/chips/mail/gmail.sh"
-GCAL="${HOME}/.config/wm_script/chips/mail/gcal.sh"
+# GMAIL="${HOME}/.config/wm_script/chips/mail/gmail.sh"
+# GCAL="${HOME}/.config/wm_script/chips/mail/gcal.sh"
 
-# $(cat ${HOME}/.config/i3/config | \
-#   grep "set \$ws${NUM}" | \
-#   awk '{printf $3}' | \
-#   sed 's/"//g")
+W2=$(cat ${HOME}/.config/i3/config | \
+     grep "set \$ws2" | \
+     awk '{printf $3}' | \
+     sed 's/"//g')
+
+W7=$(cat ${HOME}/.config/i3/config | \
+     grep "set \$ws7" | \
+     awk '{printf $3}' | \
+     sed 's/"//g')
+
+W8=$(cat ${HOME}/.config/i3/config | \
+     grep "set \$ws8" | \
+     awk '{printf $3}' | \
+     sed 's/"//g')
 
 # W2="2:"
 # W7="7:"
 # W8="8:"
 
-# i3-msg "workspace ${W7}; append_layout ${HOME}/.config/i3/7.json"
-# i3-msg "workspace ${W8}; append_layout ${HOME}/.config/i3/8.json"
-# i3-msg "workspace ${W2};"
+i3-msg "workspace ${W7}; append_layout ${HOME}/.config/i3/7.json"
+i3-msg "workspace ${W8}; append_layout ${HOME}/.config/i3/8.json"
+i3-msg "workspace ${W2};"
 
 firefox &
 
 telegram-desktop &
 signal-desktop &
-# . ${WATTA}
-# . ${SLACK}
+. ${SLACK}
+. ${WATTA}
 
 . ${TRELLO}
 . ${TUTA}
