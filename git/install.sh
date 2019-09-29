b253ec9211d624e52ea0790f3458beaffbb65a30
seintzx <seintzx@gmail.com>
2019-09-29 17:23:22 +0200

    update install script

--- git/install.sh
+++ git/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname `readlink -f "$0"`)
+FOL="${HOME}"
 
 echo -e "Copying config..\n"
-ln ${BASEDIR}/gitconfig ${HOME}/.gitconfig
+ln ${BASEDIR}/gitconfig ${FOL}/.gitconfig
 echo -e "\nDone"
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname `readlink -f "$0"`)
+FOL="${HOME}"
 
 echo -e "Installing tmux..\n"
 yay -S tmux
 
 echo -e "Copying config..\n"
-ln ${BASEDIR}/tmux.conf ${HOME}/.tmux.conf
+ln ${BASEDIR}/tmux.conf ${FOL}/.tmux.conf
 
 echo -e "\nDone"
