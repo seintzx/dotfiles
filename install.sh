8d4144158367ef398185dfc4a6be65f84f3f9b56
seintzx <seintzx@gmail.com>
2019-10-02 11:58:14 +0200

    update install script

--- install.sh
+++ install.sh
@@ -7,23 +7,37 @@ NOINSTALL=`find . -mindepth 1 -maxdepth 1 -type d '!' \
             awk -F '/' '{print $2}' | \
             uniq | sort | grep -v ".git"`
 
-for INSTALLDIR in $(ls -d *):
-do
-    if [[ -d ${BASEDIR}/${INSTALLDIR} ]]
-    then
-        if [[ -e ${BASEDIR}/${INSTALLDIR}/install.sh ]]
+install () {
+    for INSTALLDIR in $(ls -d *):
+    do
+        if [[ ${INSTALLDIR} = "yay" ]]
         then
-            ${BASEDIR}/${INSTALLDIR}/install.sh
-            # echo ${BASEDIR}/${a}/install.sh
+            echo -e "\nSkipping yay..\n"
+            break
+        else
+            INSTALLDIR="${BASEDIR}/${INSTALLDIR}"
+            if [[ -d ${INSTALLDIR} ]]
+            then
+                if [[ -e ${INSTALLDIR}/install.sh ]]
+                then
+                    # ${INSTALLDIR}/install.sh
+                    echo -e "Installing ${INSTALLDIR}"
+                fi
+            fi
         fi
-    fi
-    sleep 2
-done
+    done
+}
 
-echo -e "==== st, slock and xdg should be done manually ====\n"
-echo -e "Those folders doesn't have an install script:"
-for FOLDER in ${NOINSTALL}
-do
-    echo "- ${FOLDER}"
-done
-echo -e "You may want to check them individually"
+check_folder () {
+    echo -e "==== st, slock and xdg should be done manually ====\n"
+    echo -e "Those folders doesn't have an install script:"
+    for FOLDER in ${NOINSTALL}
+    do
+        echo "- ${FOLDER}"
+    done
+    echo -e "You may want to check them individually"
+}
+
+# ${BASEDIR}/yay/install.sh
+install
+check_folder
