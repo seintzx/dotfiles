7ef8c34312507b67f2bec8b57fcd46e03b6d847a
seintz <seintzx@gmail.com>
2024-04-12 12:52:41 +0200

    add extensions.sh

---
+++ vscodium/extensions.sh
@@ -0,0 +1,12 @@
+# To export the Visual Studio Code extension list, run
+# code --list-extensions | xargs -L 1 echo code --install-extension        
+
+code --install-extension dbaeumer.vscode-eslint &&
+code --install-extension esbenp.prettier-vscode &&
+code --install-extension jdinhlife.gruvbox &&
+code --install-extension ms-python.black-formatter &&
+code --install-extension ms-python.debugpy &&
+code --install-extension ms-python.isort &&
+code --install-extension ms-python.python &&
+code --install-extension ms-python.vscode-pylance &&
+code --install-extension ms-vsliveshare.vsliveshare
