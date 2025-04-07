4a68489cddbbec24291a26992041f571331c2ebd
seintz <seintzx@gmail.com>
2025-04-07 12:08:42 +0200

    Update extensions.sh

--- vscodium/extensions.sh
+++ vscodium/extensions.sh
@@ -14,3 +14,17 @@ code --install-extension ms-python.isort &&
 code --install-extension ms-python.python &&
 code --install-extension ms-python.vscode-pylance &&
 code --install-extension ms-vsliveshare.vsliveshare
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension googlecloudsecurity.yara-l
+code --install-extension jdinhlife.gruvbox
+code --install-extension mikestead.dotenv
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vsliveshare.vsliveshare
